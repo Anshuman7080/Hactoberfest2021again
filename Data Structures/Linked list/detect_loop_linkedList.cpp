54bcd6f55aded2473944b312b043fd696ecd609d
Anshuman7080 <164294129+Anshuman7080@users.noreply.github.com>
2024-10-06 17:43:52 +0000

    Floyd’s Cycle Detection Algorithm

--- Data Structures/Linked list/detect_loop_linkedList.cpp
+++ Data Structures/Linked list/detect_loop_linkedList.cpp
@@ -1,57 +1,53 @@
-// C++ program to detect loop in a linked list
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 struct Node {
-	int data;
-	struct Node* next;
+    int data;
+    Node* next;
 };
 
-void push(struct Node** head_ref, int new_data)
-{
-	
-	struct Node* new_node = new Node;
-
-	new_node->data = new_data;
-
-	new_node->next = (*head_ref);
-	(*head_ref) = new_node;
+// Function to push a new node onto the linked list
+void push(Node** head_ref, int new_data) {
+    Node* new_node = new Node;
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
 }
 
-bool detectLoop(struct Node* h)
-{
-	unordered_set<Node*> s;
-	while (h != NULL) {
-		
-		if (s.find(h) != s.end())
-			return true;
+// Function to detect a loop in the linked list
+bool detectLoop(Node* head) {
+    if (head == nullptr) return false;
 
+    Node* slow = head;   // Tortoise
+    Node* fast = head;   // Hare
 
-		s.insert(h);
+    while (fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;           // Move slow pointer by 1
+        fast = fast->next->next;     // Move fast pointer by 2
 
-		h = h->next;
-	}
-
-	return false;
+        // If they meet at some point, then there is a loop
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false; // No loop found
 }
 
-int main()
-{
-	
-	struct Node* head = NULL;
+int main() {
+    Node* head = nullptr;
 
-	push(&head, 20);
-	push(&head, 4);
-	push(&head, 15);
-	push(&head, 10);
+    push(&head, 20);
+    push(&head, 4);
+    push(&head, 15);
+    push(&head, 10);
 
-	head->next->next->next->next = head;
+    // Creating a loop for testing
+    head->next->next->next->next = head;
 
-	if (detectLoop(head))
-		cout << "Loop found";
-	else
-		cout << "No Loop";
+    if (detectLoop(head))
+        cout << "Loop found";
+    else
+        cout << "No Loop";
 
-	return 0;
+    return 0;
 }
-
